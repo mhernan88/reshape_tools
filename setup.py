6ee265b429004615693a34533b840ef14b92b0d3
Michael Hernandez <mhernan88@gmail.com>
2020-11-24 13:06:27 -0500

    Change to setup.py to recognize packages

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version="0.1.1",
     packages=[
         "reshape_tools",
+        "reshape_tools.make_recurrent",
+        "reshape_tools.get_date_component",
         "sample_data"
     ],
     install_requires=["pandas", "numpy", "nptyping"],
