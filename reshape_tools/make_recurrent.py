c34bbe29619d7ec6913eebc7c1b400e57b25b1c0
Michael Hernandez <mhernan88@gmail.com>
2020-11-24 13:33:19 -0500

    Migrated get_date_component to make_recurrent.py

--- reshape_tools/make_recurrent.py
+++ reshape_tools/make_recurrent.py
@@ -4,6 +4,31 @@ from nptyping import NDArray
 from typing import Any, Optional
 
 
+GDC_TRANSFORMATIONS = {
+    "m": lambda x: x.dt.to_period("M"),
+    "month": lambda x: x.dt.to_period("M"),
+    "d": lambda x: x.dt.to_period("D"),
+    "day": lambda x: x.dt.to_period("D"),
+    "y": lambda x: x.dt.to_period("Y"),
+    "year": lambda x: x.dt.to_period("Y"),
+}
+
+
+def get_date_component(dates: pd.Series, component: str) -> pd.Series:
+    """Returns dates aggregated at day, month, or year level.
+
+    Args:
+        dates (pd.Series): A series of dates to transform.
+        component (str): The component you wish to pull.
+
+    Returns:
+        A series of the components.
+    """
+    gdc_transformation = GDC_TRANSFORMATIONS[component.lower()]
+    periods = gdc_transformation(dates)
+    return periods.astype(str)
+
+
 def make_recurrent(
     df: pd.DataFrame,
     time_window: int,
