9e5c07a876f65a0eac313952f541c0be567bafad
Michael Hernandez <mhernan88@gmail.com>
2020-11-24 16:56:12 -0500

    Updated logger

--- reshape_logging/loggers.py
+++ reshape_logging/loggers.py
@@ -2,16 +2,27 @@ from logging import Logger, getLogger, DEBUG, INFO, WARNING, ERROR
 from typing import Optional
 
 def fmt(msg, *args, **kwargs):
-    args_str = ", ".join(args)
-    args_str = f"args: {args_str}"
+    args_str = None
+    if args is not None:
+        if len(args) > 0:
+            args_str = ", ".join(args)
+            args_str = f"args: {args_str}"
+        
     
-    kwargs_list = []
-    for k, v in kwargs.items():
-        kwargs_list.append(f"{k}={v}")
-    kwargs_str = ", ".join(kwargs_list)
-    kwargs_str = f"kwawrgs: {kwargs_str}"
+    kwargs_str = None
+    if kwargs is not None:
+        if len(list(kwargs.keys())) > 0:
+            kwargs_list = []
+            for k, v in kwargs.items():
+                kwargs_list.append(f"{k}={v}")
+            kwargs_str = ", ".join(kwargs_list)
+            kwargs_str = f"kwawrgs: {kwargs_str}"
 
-    return f"{msg}; {args_str}; {kwargs_str}"
+    if args_str is not None:
+        msg += f"; {args_str}"
+    if kwargs_str is not None:
+        msg += f"; {kwargs_str}"
+    return msg
 
 class CustomLogger(Logger):
     def debug(self, msg, *args, **kwargs):
@@ -26,6 +37,6 @@ class CustomLogger(Logger):
         if self.isEnabledFor(WARNING):
             self._log(WARNING, fmt(msg, *args, **kwargs), tuple())
 
-    def error_kv(self, msg, *args, **kwargs):
+    def error(self, msg, *args, **kwargs):
         if self.isEnabledFor(ERROR):
             self._log(ERROR, fmt(msg, *args, **kwargs), tuple())
